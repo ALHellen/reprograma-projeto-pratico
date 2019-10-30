b1b23f6fa6e8261673642e3fa4b9dc04a93d3505
ALHellen <hellen.abade@outlook.com>
2019-10-30 20:32:50 -0300

    Buscando concluidos e colaboradoras - exercício extra

--- src/controllers/listaController.js
+++ src/controllers/listaController.js
@@ -7,7 +7,7 @@ exports.get = (req, res) => {
     res.status(200).send(lista)
 }
 
-exports.getById = (req, res) => {
+exports.getById = (req, res) => { 
     const id = req.params.id
     const list = listas.find(list => list.id == id)
     // delete lista.id
@@ -15,6 +15,20 @@ exports.getById = (req, res) => {
     res.status(200).send(list)
 }
 
+exports.getByConcluido = (req, res) => { 
+    let concluido = listas.filter((busca) => {
+    return busca.concluido === 'true';
+      })
 
+      console.log(concluido,"SEI LA")
+      res.status(200).send(concluido)
+}
 
+exports.getByColab = (req, res) => { 
+    const colab = req.params.colaboradora
+    const  lista = listas.find(list => list.colab == colab)
+    // delete lista.id
+    console.log(colab,"SEI LA")
+    res.status(200).send(lista)
+}
 
--- src/routes/listaRoute.js
+++ src/routes/listaRoute.js
@@ -2,7 +2,11 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controllers/listaController")
 
+//ROTAS TAMBÉM SÃO LIDAS EM SEQUENCIA --- WARNING
+
 router.get("/", controller.get)
+router.get("/:colaborador", controller.getByColab)
+router.get("/:concluido", controller.getByConcluido)
 router.get("/:id", controller.getById)
 
 module.exports = router
